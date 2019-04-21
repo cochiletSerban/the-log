be7589c4b82e8adf479b44ade66f41e435f5f007
cochiletSerban <cochiletSerban@gmail.com>
2019-04-21 14:54:50 +0300

    added footer that works noww

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,5 @@ export class AppComponent implements OnInit {
   title = 'app';
   ngOnInit() {
   }
+  constructor(public router: Router) {}
 }
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,18 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy, ElementRef, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
+declare var $: any;
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  constructor(public router: Router) { }
+  constructor(public router: Router, private elRef: ElementRef) { }
 
-  ngOnInit() {
-    console.log(this.router.config);
+  ngAfterViewInit() {
 
+
+    // reset
+    $('html, body').css('height', 'initial');
+
+    // add the good stuff to make it sticky
+    $('html').css('position', 'relative');
+    $('html').css('min-height', '100%');
+    $('body').css('padding-bottom', $('footer').height() + 50);
+
+    // add the good stuff make it responsive
+    $( window ).resize(function() {
+       $('body').css('padding-bottom', $('footer').height() + 50);
+    });
+
+  }
+
+  ngOnDestroy(): void {
+    $('html, body').css('height', '100%');
+    $('body').css('padding-bottom', 'initial');
   }
 
 }
